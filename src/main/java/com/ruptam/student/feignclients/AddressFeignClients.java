61318e0131f68425023d36136c8dacde53bdd045
Ruptam <ruptam25@outlook.com>
2022-02-17 16:05:52 +0530

    Configured feign client to call internally address service using api-gateway

--- src/main/java/com/ruptam/student/feignclients/AddressFeignClients.java
+++ src/main/java/com/ruptam/student/feignclients/AddressFeignClients.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "address-service", path = "/address")
+@FeignClient(value = "api-gateway")
 public interface AddressFeignClients {
 
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/address-service/address/")
     public AddressDTO getAddressById(@RequestParam("addressId") Long addressId);
     
 }
